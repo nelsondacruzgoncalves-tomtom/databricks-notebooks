3eea9375708f4d9e7c69b46ad7962277c457e32c
nelson.dacruzgoncalves@tomtom.com <nelson.dacruzgoncalves@tomtom.com>
2022-10-11 11:24:06 +0000

    remove commented code

--- test_myfunctions.py
+++ test_myfunctions.py
@@ -8,11 +8,3 @@ def test_getTables():
 def test_isEven():
     assert myfunctions.isEven(2) is True
     assert myfunctions.isEven(3) is False
-
-# Does the column exist?
-#def test_columnExists():
-#      assert myfunctions.columnExists(tableName, dbName, columnName) is True
-
-# Is there at least one row for the value in the specified column?
-#def test_numRowsInColumnForValue():
-#      assert myfunctions.numRowsInColumnForValue(tableName, dbName, columnName, columnValue) > 0
